5216c18ffcaa9b64b5b94a0a7b49f8c78e8c2086
kaike torres <kaiketorresdasilva@gmail.com>
2025-01-15 22:55:37 -0300

    Adicionado o conhecimento sobre os tipos primitivos em Java, incluindo: - **int**: números inteiros (32 bits). - **double**: números de ponto flutuante de precisão dupla (64 bits). - **float**: números de ponto flutuante de precisão simples (32 bits). - **char**: caracteres (16 bits, Unicode). - **byte**: inteiros pequenos (8 bits). - **short**: inteiros de tamanho reduzido (16 bits). - **long**: inteiros maiores (64 bits). - **boolean**: valores lógicos (`true` ou `false`).
    Esses tipos são fundamentais para a manipulação de dados em Java.

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -2,6 +2,7 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aula02TiposPrimitivos {
     public static void main(String[] args){
+
         // int, double, float, char, byte, short, long, boolean
 
         int idade = 10;
