b58117282d603d21caf2ebfd29ea74cdc5c7b4b7
kaike torres <kaiketorresdasilva@gmail.com>
2025-01-17 19:45:04 -0300

    feat: implementar operações aritméticas básicas
    - Criadas variáveis `numero01` e `numero02` para armazenar os operandos.
    - Calculadas operações de soma, subtração, multiplicação e divisão usando as variáveis.
    - Exibidos os resultados de cada operação no console.
    - Estruturado o código para demonstrar o uso de operadores aritméticos em Java.

---
+++ src/academy/devdojo/maratonajava/introducao/Aula04operadores.java
@@ -0,0 +1,31 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula04operadores {
+
+    public static void main(String[] args) {
+
+        // + - / *
+
+        int numero01 = 30;
+
+        int numero02 = 20;
+
+        int soma = numero01 + numero02;
+
+        int subtracao = numero01 - numero02;
+
+        int multiplicacao = numero01 * numero02;
+
+        int divisao = numero01 / numero02;
+
+        System.out.println(soma);
+        System.out.println(subtracao);
+        System.out.println(multiplicacao);
+        System.out.println(divisao);
+    }
+
+
+
+
+
+}
