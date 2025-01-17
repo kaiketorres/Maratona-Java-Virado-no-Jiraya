ee080b455feb56aeaa201f8b54090b6e224a2959
kaike torres <kaiketorresdasilva@gmail.com>
2025-01-16 21:48:02 -0300

    - Adicionada a classe `Aula02TiposPrimitivos` no pacote `academy.devdojo.maratonajava.introducao`. - Implementados os principais tipos primitivos em Java, incluindo:   - `int` para valores inteiros.   - `long` para inteiros grandes.   - `double` e `float` para números de ponto flutuante.   - `byte` e `short` para valores inteiros menores.   - `boolean` para valores lógicos.   - `char` para caracteres Unicode. - Adicionado código para imprimir exemplos no console.

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -7,6 +7,26 @@ public class Aula02TiposPrimitivos {
 
         int idade = 10;
 
+        long numeroGrande = 100000;
+
+        double salarioDouble = 2000;
+
+        float salarioFloat = 2500;
+
+        byte idadeByte = 127;
+
+        short idadeShort = 15;
+
+        boolean verdadeiro = true;
+
+        boolean falso = false;
+
+        char caractere = 65 ;
+
+
+
         System.out.println("A idade e " + idade);
+        System.out.println(verdadeiro);
+        System.out.println(caractere);
     }
 }
