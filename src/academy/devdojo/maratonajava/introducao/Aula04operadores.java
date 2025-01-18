8839a8c0894356b9e4b5b549eae458f6e403224d
kaike torres <kaiketorresdasilva@gmail.com>
2025-01-18 15:24:11 -0300

    feat: adiciona exemplos de operadores aritméticos e de incremento/decremento em Java
    - Demonstrados operadores de atribuição (+=, -=, *=, /=, %=)
    - Implementados exemplos de incremento (++) e decremento (--) com variáveis
    - Exibido o valor das variáveis após as operações no console

--- src/academy/devdojo/maratonajava/introducao/Aula04operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Aula04operadores.java
@@ -67,6 +67,34 @@ public class Aula04operadores {
         boolean isPlastationCincoCompravel = valorTotalContaCorrente > valorPlaystation || valorTotalContaPoupanca > valorPlaystation;
         System.out.println("isPlastationCincoCompravel" + isPlastationCincoCompravel);
 
+        // , += -= *= /= %=
+
+        double bonus = 1800;
+
+        bonus = bonus + 1000;
+        bonus += 1000;
+        bonus -= 1000;
+        bonus *= 2;
+        bonus /= 2;
+        bonus %= 2;
+
+        System.out.println(bonus);
+
+        // ++ --
+
+        int contador = 0;
+
+        contador += 1;
+        contador++;
+        contador--;
+
+        System.out.println(contador);
+
+        int contador2 = 0;
+
+        System.out.println(contador2++);
+        System.out.println(contador2);
+
 
     }
 }
