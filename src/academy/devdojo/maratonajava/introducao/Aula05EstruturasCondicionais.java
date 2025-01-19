4d8eddf84bec1c415537d292548828b471702868
kaike torres <kaiketorresdasilva@gmail.com>
2025-01-19 02:34:17 -0300

    feat: implementei exemplos de estruturas condicionais em Java
    - Verifiquei se uma pessoa está autorizada a comprar bebida alcoólica com base na idade
    - Usei estruturas `if` para validar condições booleanas
    - Mostrei um exemplo de prática incorreta (atribuição dentro de um `if`) para reforçar boas práticas
    - Adicionei mensagens no console para exibir os resultados das verificações

---
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais.java
@@ -0,0 +1,26 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais {
+    public static void main(String[] args) {
+
+        int idade = 15;
+
+        boolean isAutorizadoComprarBebida = idade >= 18;
+
+        if(isAutorizadoComprarBebida){
+            System.out.println("Autorizado a comprar bebida alcólica");
+        }
+
+        if(isAutorizadoComprarBebida == false){
+            System.out.println("Não Autorizado a comprar bebida alcólica");
+        }
+
+        boolean c = true;
+
+        if(c = false){
+            System.out.println("Dentro de algo que nunca deve ser feito");
+        }
+
+        System.out.println("Fora do if");
+    }
+}
