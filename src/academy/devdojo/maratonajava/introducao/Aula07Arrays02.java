d5856989fa35f02de002f40194f67ac44350a40f
kaike torres <kaiketorresdasilva@gmail.com>
2025-01-21 23:55:41 -0300

    feat: exemplo de criação e iteração em array de inteiros em Java
    - Criei um array de inteiros chamado `idades` com 3 elementos
    - Inicializei os valores do array com idades específicas
    - Usei um loop `for` para iterar sobre o array e imprimir seus valores

---
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula07Arrays02 {
+
+    public static void main(String[] args) {
+
+
+        int [] idades = new int[3];
+
+        idades[0] = 21;
+        idades[1] = 12;
+        idades[2] = 11;
+
+
+        for (int i = 0; i < idades.length ;i++){
+            System.out.println(idades[i]);
+        }
+
+
+    }
+}
