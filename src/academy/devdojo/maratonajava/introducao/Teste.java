1d2ac182f95dfa43ba9509172507d0e7c4d4b572
kaike <kaiketorresdasilva@gmail.com>
2025-01-25 21:44:36 -0300

    Criei a classe Estudante e realizei testes com objetos
    Implementei a classe `Estudante` no pacote `dominio`, adicionando os atributos `nome`, `idade` e `sexo`. Também desenvolvi a classe `EstudanteTest01` no pacote `test` para testar a funcionalidade dessa classe.
    
    No teste, criei dois objetos do tipo `Estudante`. O primeiro objeto, chamado `estudante`, foi inicializado com os valores `nome = "kaike"`, `idade = 21` e `sexo = 'M'`. Já o segundo objeto, `estudante2`, foi usado para verificar o comportamento padrão do atributo `nome` ao ser modificado.
    
    Por fim, exibi os atributos dos objetos no console, garantindo que cada instância pudesse armazenar dados de forma independente.

--- src/academy/devdojo/maratonajava/introducao/Teste.java
+++ src/academy/devdojo/maratonajava/introducao/Teste.java
@@ -4,22 +4,19 @@ public class Teste {
     public static void main(String[] args){
 
 
-        String [] nomes = new String[3];
+        String [] [] nomes = new String[3] [3];
 
-        nomes[0] = "mario";
-        nomes[1] = "lucas";
-        nomes[2] = "kaike";
+        nomes[0][0] = "kaike";
+        nomes[1][1] = "gustavo";
+        nomes[2][2] = "thon";
 
-
-        for (int i = 0; i < nomes.length; i++ ){
-            System.out.println(nomes[i]);
+        for (int i = 0; i < nomes.length ; i++) {
+            for (int j = 0; j < nomes[2].length ; j++) {
+                System.out.println(nomes[i][i]);
+            }
         }
 
-
-        for(String i :nomes){
-            System.out.println(i);
-        }
-
+        System.out.println(nomes[2][2]);
 
 
     }
