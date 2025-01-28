73d8a5cb588c3dd9230656f60650158769927d9a
kaike <kaiketorresdasilva@gmail.com>
2025-01-27 23:59:02 -0300

    feat: Adiciona métodos para somar números na classe Calculadora
    - **Novos métodos adicionados:**
      - `somaArray`: Recebe um array de inteiros e calcula a soma de seus elementos, imprimindo o resultado.
      - `somaVarArgns`: Usa VarArgs para permitir que múltiplos inteiros sejam passados como parâmetros, calculando e imprimindo a soma.
    
    - **Objetivo das alterações:**
      - Fornecer flexibilidade no cálculo de somas:
        - `somaArray`: Ideal para situações em que os números estão em um array.
        - `somaVarArgns`: Permite passar diretamente os números sem necessidade de criar um array.

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodosdominio/Aintroducaoclasses/dominio/Calculadora.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodosdominio/Aintroducaoclasses/dominio/Calculadora.java
@@ -55,5 +55,30 @@ public class Calculadora {
     }
 
 
+    public void somaArray(int [] numeros){
+        int soma = 0;
+
+        for (int num: numeros){
+            soma += num;
+        }
+
+        System.out.println(soma);
+
+
+    }
+
+
+
+    public  void somaVarArgns(int... numeros){
+        int soma = 0;
+
+        for (int num: numeros){
+            soma += num;
+        }
+
+        System.out.println(soma);
+
+
+    }
 
 }
