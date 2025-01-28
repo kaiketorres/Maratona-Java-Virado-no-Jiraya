d7bb52ea3199b0f221a24bf092cb0c1bcbe93991
kaike <kaiketorresdasilva@gmail.com>
2025-01-27 23:59:44 -0300

    feat: Adiciona método de impressão na classe Calculadora
    - Adicionado o método `imprime`, que exibe os valores dos atributos `nome`, `idade` e `sexo` da classe.
    - Objetivo: Facilitar a visualização das informações de uma instância da classe.

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodosdominio/Aintroducaoclasses/dominio/Estudante.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodosdominio/Aintroducaoclasses/dominio/Estudante.java
@@ -1,7 +1,18 @@
 package academy.devdojo.maratonajava.javacore.Bintroducaometodosdominio.Aintroducaoclasses.dominio;
 
 public class Estudante {
-    public String nome = "Zoro";
+
+    public String nome;
     public int idade;
     public char sexo;
+
+
+    public void imprime(){
+
+        System.out.println(this.nome);
+        System.out.println(this.idade);
+        System.out.println(this.sexo);
+
+
+    }
 }
