084d3c3f132ec92573af0ecd730415176f3b3752
kaike <kaiketorresdasilva@gmail.com>
2025-01-30 21:10:10 -0300

    feat: Adiciona classe Pessoa01 com controle de saldo e idade
    - Implementa a classe Pessoa01 para representar uma pessoa com saldo e idade.
    - Adiciona os métodos setters para definir os valores de saldo e idade.
    - Adiciona os métodos getters para recuperar os valores de saldo e idade.
    - Essa classe será utilizada no sistema de compra de jogos para validar saldo e classificação etária."

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodosdominio/Aintroducaoclasses/dominio/Pessoa01.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodosdominio.Aintroducaoclasses.dominio;
+
+public class Pessoa01 {
+
+    private float saldo;
+    private int idade;
+
+    public void setSaldo(float saldo){
+        this.saldo = saldo;
+    }
+
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+
+    public float getSaldo(){
+        return saldo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+}
