24d059a784f16ceffd490140ccc1e5c90c920004
kaike <kaiketorresdasilva@gmail.com>
2025-02-03 23:24:51 -0300

    test: adicionar teste de construtores para inicializar anime
    - Criado teste para os construtores da classe Anime.
    - Utilizado o construtor que recebe nome, tipo, número de episódios e gênero para inicializar o anime.
    - Adicionada chamada para o método `imprime` para exibir os dados do anime.

---
+++ src/academy/devdojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.Dconstrutores.test;
+
+
+import academy.devdojo.maratonajava.javacore.Dconstrutores.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+
+
+        Anime anime = new Anime("Haikyuu","Tv",13,"ação");
+
+        anime.imprime();
+
+
+    }
+}
