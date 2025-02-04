5b4209104ca4d449be0185f8bbdb4f936d7ceac6
kaike <kaiketorresdasilva@gmail.com>
2025-02-03 23:24:46 -0300

    test: adicionar teste de sobrecarga de métodos para inicializar anime
    - Criado teste para a sobrecarga de métodos na classe Anime.
    - Utilizado o método `init` para inicializar os dados do anime com nome, tipo, número de episódios e gênero.
    - Adicionada chamada para o método `imprime` para exibir os dados do anime.

---
+++ src/academy/devdojo/maratonajava/javacore/Csobrecargametodos/test/AnimeTest01.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.javacore.Csobrecargametodos.test;
+
+import academy.devdojo.maratonajava.javacore.Csobrecargametodos.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+
+        Anime anime = new Anime();
+
+
+        anime.init("Demon slayer","Tv",65,"ação");
+
+
+        anime.imprime();
+
+
+
+
+    }
+}
