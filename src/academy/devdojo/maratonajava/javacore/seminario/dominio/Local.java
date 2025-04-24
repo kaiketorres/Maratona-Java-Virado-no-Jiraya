c11255eabb6adcef65a5c1b4b9a7c6c2ba062981
kaiketorres <kaiketorresdasilva@gmail.com>
2025-04-24 00:08:49 -0300

    Class com atributo endereco, uitlizada no projeto de seminario

---
+++ src/academy/devdojo/maratonajava/javacore/seminario/dominio/Local.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.seminario.dominio;
+
+public class Local {
+
+    private String endereco;
+
+    public Local(String endereco) {
+        this.endereco = endereco;
+    }
+
+}
